006047fc758ca4bbcfff652eef9097c4fa07e6f6
wesleydecezere <wesleydecezere@gmail.com>
2021-10-10 15:06:49 -0300

    Add header component

---
+++ pwa-duodigit/src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text } from 'bold-ui';
+
+export const Header = () => (
+  <div>
+    <Text variant="h1"> Calculadora de menor múltiplo duodígito </Text>
+    <Text variant="main">Descrição da aplicação.</Text>
+  </div>
+);
