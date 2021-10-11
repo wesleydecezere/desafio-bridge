7ea71cec9b72064e81df6893382548b409d85e40
wesleydecezere <wesleydecezere@gmail.com>
2021-10-11 15:49:53 -0300

    Test Home component

---
+++ pwa-duodigit/src/views/Home.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Home from './Home';
+
+test('renders an input field, a table and a button', () => {
+  render(<Home />);
+  const inputEl = screen.getByRole('spinbutton');
+  const tableEl = screen.getByRole('table');
+  const buttonEl = screen.getByRole('button');
+  expect(inputEl).toBeInTheDocument();
+  expect(tableEl).toBeInTheDocument();
+  expect(buttonEl).toBeInTheDocument();
+});
+test.only('when types an number into input and clicks on the button, adds a non empty row to the table', () => {
+  render(<Home />);
+  const inputEl = screen.getByRole('spinbutton');
+  const buttonEl = screen.getByRole('button');
+  const emptyRowEl = screen.queryByRole('row', { name: 'Nenhum registro encontrado' });
+  expect(emptyRowEl).toBeInTheDocument();
+
+  fireEvent.input(inputEl, '999');
+  fireEvent.click(buttonEl);
+  expect(emptyRowEl).not.toBeInTheDocument();
+});
