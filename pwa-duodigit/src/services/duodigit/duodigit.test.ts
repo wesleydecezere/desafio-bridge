664f749f6390bac810700bd4ac3f869cf1d618b5
wesleydecezere <wesleydecezere@gmail.com>
2021-10-12 16:41:39 -0300

    Improve duodigit service test

--- pwa-duodigit/src/services/duodigit/duodigit.test.ts
+++ pwa-duodigit/src/services/duodigit/duodigit.test.ts
@@ -1,18 +1,17 @@
-import { runPost, PostResponse, checkSmallestDuodigitMultiple } from './duodigit';
-import { RowType } from '../../types';
+import { runPost, checkSmallestDuodigitMultiple } from './duodigit';
 
 jest.setTimeout(10000);
 
 describe('POST', () => {
-  test('/duodigit resolves with ok property as true and data property as result of operation when passes 999 as data', () => {
+  test('/duodigit resolves with ok property as true and data property as correct result of operation when passes 999 as data', () => {
     const data = { number: 999 };
     const route = '/duodigit';
 
-    return runPost(route, data).then((res) => res as PostResponse<RowType>).then((res) => {
+    return runPost(route, data).then((res) => res).then((res) => {
       expect(res.ok).toBeTruthy();
       expect(res.data.number).toBe(999);
       expect(res.data.duodigit).toBe(9990);
-      expect(res.data.time).not.toBe(0);
+      expect(res.data.time).toBeGreaterThan(0);
     });
   });
   test('/duodigit resolves with ok property as false when passes 8910 as data', () => {
@@ -29,15 +28,15 @@ describe('POST', () => {
   });
 });
 describe('Check smallest duodigit multiple service', () => {
-  test('rejects with an error when passes number 8910', () => {
-    const number = 8910;
+  test('rejects with an error when passes number 8910', () => expect(
+    checkSmallestDuodigitMultiple(8910),
+  ).rejects.toThrowError());
 
-    return expect(checkSmallestDuodigitMultiple(number)).rejects.toThrowError();
-  });
-  test('resolves with a RowType object when passes number 999', () => {
-    const number = 999;
-
-    return expect(checkSmallestDuodigitMultiple(number).then((res) => (res as RowType).duodigit))
-      .resolves.toEqual(9990);
+  test('resolves with a RowType object when passes number 999', async () => {
+    await checkSmallestDuodigitMultiple(999).then((res) => {
+      expect(res).toHaveProperty('number');
+      expect(res).toHaveProperty('duodigit');
+      expect(res).toHaveProperty('time');
+    });
   });
 });
