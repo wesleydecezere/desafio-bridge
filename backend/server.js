da6b8c5eda142293280c7c249a44383c42df9304
Wesley Decezere <wesleydecezere@gmail.com>
2022-04-20 21:09:44 -0300

    Get PORT env var

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const isDuodigit = (number) => new Set(number.toString().replace(/\D/g, '')).size <= 2;
 
 app.get('/', (req, res) => {
